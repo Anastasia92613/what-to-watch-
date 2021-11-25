a6e21ac13c22d8a0418f54aa348ccbd8e13867e3
Anastasia92613 <54165384+Anastasia92613@users.noreply.github.com>
2021-11-25 18:57:53 +0300

    ДЗ 2- прописывание роутов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import Main from '../main/main';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import MoviePage from '../movie-page/movie-page';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
+import ErrorPage from '../error-page/error-page';
 
 const App = ({movieCards}) => {
   return (
     <>
-      <Main movieCards={movieCards} />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Main movieCards={movieCards} />
+          </Route>
+          <Route path="/login" exact>
+            <SignIn />
+          </Route>
+          <Route path="/mylist" exact>
+            <MyList />
+          </Route>
+          <Route path="/films/:id" exact>
+            <MoviePage />
+          </Route>
+          <Route path="/films/:id/review" exact>
+            <AddReview />
+          </Route>
+          <Route path="/player/:id" exact>
+            <Player />
+          </Route>
+          <Route>
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
